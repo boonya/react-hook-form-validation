b38a9663aa4de4d3706face3eeb3ca0afeec3b82
Serhii [boonya] Buinytskyi <779184+boonya@users.noreply.github.com>
2022-01-16 15:47:47 +0100

    one more fix

--- src/validators/index.test.ts
+++ src/validators/index.test.ts
@@ -1,33 +1,27 @@
 import aggregator from './index';
 import { VALIDATORS } from '../types';
-import {isValid as required} from './required';
-import {isValid as min} from './min';
-import {isValid as max} from './max';
-import {isValid as minLength} from './minLength';
-import {isValid as maxLength} from './maxLength';
-import {isValid as email} from './email';
-import {isValid as url} from './url';
-import {isValid as pattern} from './pattern';
-import {isValid as func} from './func';
+import { isValid as required } from './required';
+import { isValid as min } from './min';
+import { isValid as max } from './max';
+import { isValid as email } from './email';
+import { isValid as url } from './url';
+import { isValid as pattern } from './pattern';
+import { isValid as func } from './func';
 
 jest.mock('./required');
 jest.mock('./min');
 jest.mock('./max');
-jest.mock('./minLength');
-jest.mock('./maxLength');
 jest.mock('./email');
 jest.mock('./url');
 jest.mock('./pattern');
 jest.mock('./func');
 
-const validators = { required, min, max, minLength, maxLength, email, url, pattern, func };
+const validators = { required, min, max, email, url, pattern, func };
 
 beforeEach(() => {
 	jest.mocked(required).mockName(VALIDATORS.required).mockReturnValue(VALIDATORS.required);
 	jest.mocked(min).mockName(VALIDATORS.min).mockReturnValue(VALIDATORS.min);
 	jest.mocked(max).mockName(VALIDATORS.max).mockReturnValue(VALIDATORS.max);
-	jest.mocked(minLength).mockName(VALIDATORS.minLength).mockReturnValue(VALIDATORS.minLength);
-	jest.mocked(maxLength).mockName(VALIDATORS.maxLength).mockReturnValue(VALIDATORS.maxLength);
 	jest.mocked(email).mockName(VALIDATORS.email).mockReturnValue(VALIDATORS.email);
 	jest.mocked(url).mockName(VALIDATORS.url).mockReturnValue(VALIDATORS.url);
 	jest.mocked(pattern).mockName(VALIDATORS.pattern).mockReturnValue(VALIDATORS.pattern);
